f43c61ec35dd26f2959c87d21226c9ef284fda7f
Kadir Kahraman <kadirkahraman.it@gmail.com>
2025-12-28 23:09:51 +0100

    Update recipe.js

--- recipe.js
+++ recipe.js
@@ -1,1 +1,30 @@
-let value = ["Tomate", "Teig", "Salat", "Knoblauch"];
+let value = ["Aubergine", "g Hackfleisch", "kleine Zwiebel", "kleine Tomate", "kleine grüne Paprika", "Knoblauchzehe", "EL Öl", "Salz, Pfeffer", "Paprikapulver", "Petersilie"];
+let amout = [1, 100, 1, 1, 1, 1,];
+let portion = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+
+function amoutPortion() {
+    for(let i = 0; i < portion.length; i++){
+        let sum = portion[i];
+        amout += sum;
+    }
+    return amout;
+}
+
+function ingredients() {
+    for(let i = 0; i < value.length; i++){
+
+    }
+}
+
+let karniyarikValue = [
+    {amout: 1, unit: "stk", ingridient: "Aubergine" }, 
+    {amout: 100, unit: "g", ingridient: "Hackfleisch" }, 
+    {amout: 1, unit: "stk", ingridient: "kleine Zwiebel" }, 
+    {amout: 1, unit: "stk", ingridient: "kleine Tomate" }, 
+    {amout: 1, unit: "stk", ingridient: "kleine grüne Paprika" }, 
+    {amout: 1, unit: "stk", ingridient: "Knoblauchzehe" }, 
+    {amout: 1, unit: "stk", ingridient: "EL Öl" }, 
+    { ingridient: "Salz, Pfeffer" }, 
+    { ingridient: "Paprikapulver" },
+    { ingridient: "Petersilie" },
+];
